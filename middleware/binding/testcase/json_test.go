d544e47fbb3268ebf61e0cbd07db773e08fbb267
esonhugh <esonhughoutside@gmail.com>
2025-08-22 19:09:03 +0800

    feat: add JSON binding middleware and corresponding tests

---
+++ middleware/binding/testcase/json_test.go
@@ -0,0 +1,28 @@
+package testcase
+
+import (
+	"fmt"
+	"github.com/juanjiTech/jin"
+	"github.com/juanjiTech/jin/middleware/binding"
+	"github.com/juanjiTech/jin/render"
+	"io"
+	"testing"
+)
+
+type ExampleReq struct {
+	Name string `json:"name"`
+}
+
+func TestJinBind(t *testing.T) {
+	engine := jin.New()
+	engine.POST("/json/1", binding.JSON(ExampleReq{}), func(ctx *jin.Context, req ExampleReq) {
+		_ = render.WriteJSON(ctx.Writer, req.Name)
+		fmt.Println("Received Name: ", req.Name)
+		buf, err := io.ReadAll(ctx.Request.Body)
+		if err != nil {
+			fmt.Printf("Failed to read request body: %v\n", err)
+		}
+		fmt.Println("Request Body:", string(buf), "End")
+	})
+	engine.Run(":8080")
+}
