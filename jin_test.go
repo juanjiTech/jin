a853048dc51bb23b6f66c7e251493497896297b2
esonhugh <esonhughoutside@gmail.com>
2025-11-26 15:28:59 +0800

    feat: enhance error handling in context and add corresponding tests

--- jin_test.go
+++ jin_test.go
@@ -1,10 +1,12 @@
 package jin
 
 import (
-	"github.com/juanjiTech/inject/v2"
+	"fmt"
 	"net/http"
 	"strconv"
 	"testing"
+
+	"github.com/juanjiTech/inject/v2"
 )
 
 func TestDefaultEngine(t *testing.T) {
@@ -23,6 +25,31 @@ func TestDefaultEngine(t *testing.T) {
 		v++
 		_, _ = c.Writer.WriteString(strconv.Itoa(v))
 	})
+
+	respHandler := func(c *Context, v string, err error) {
+		t.Log("response handler:", v)
+		if err != nil {
+			t.Log("response error:", err)
+			_, _ = c.Writer.WriteString("error: " + err.Error())
+			return
+		}
+		_, _ = c.Writer.WriteString(v)
+		return
+	}
+
+	e.GET("/pong", func(v int) string {
+		t.Log("pong", v)
+		return "pong " + strconv.Itoa(v)
+	}, func(c string) {
+		t.Log("final", c)
+	}, respHandler)
+	e.GET("/error", func() (string, error) {
+		return "", fmt.Errorf("damn this is an error!")
+	}, respHandler)
+	e.GET("/noerror", func() (string, error) {
+		return "no error!", nil
+	}, respHandler)
+
 	e.GET("/panic", func() {
 		panic("I am panic")
 	})
